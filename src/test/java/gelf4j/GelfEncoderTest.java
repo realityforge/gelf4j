587bf33e6663d27d8fa0f625f71f52c56fd4c27f
Peter Donald <peter@realityforge.org>
2012-04-24 22:13:08 +1000

    Add some tests around encoding messages

--- src/test/java/gelf4j/GelfEncoderTest.java
+++ src/test/java/gelf4j/GelfEncoderTest.java
@@ -4,11 +4,134 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import org.json.simple.JSONValue;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GelfEncoderTest
 {
+  @Test
+  public void fullWorkflow()
+    throws Exception
+  {
+    final GelfMessage message = new GelfMessage();
+    message.setShortMessage( "MyShortMessage" );
+    final List<byte[]> packets = new GelfEncoder( "localhost", true ).encode( message );
+    assertEquals( 1, packets.size() );
+  }
+
+  @Test
+  public void messageToJson_MissingShortMessage()
+    throws Exception
+  {
+    final GelfMessage message = new GelfMessage();
+    assertNull( new GelfEncoder( "localhost", true ).toJson( message ) );
+  }
+
+  @Test
+  public void messageToJson_WithDefaults()
+    throws Exception
+  {
+    final GelfMessage message = new GelfMessage();
+    final String myShortMessage = "MyShortMessage";
+    message.setShortMessage( myShortMessage );
+    final String json = new GelfEncoder( "localhost", true ).toJson( message );
+    assertNotNull( json );
+    final Map<String,Object> object = (Map<String,Object>) JSONValue.parse( json );
+    
+    assertEquals( "1.0", object.get( "version" ) );
+    assertEquals( myShortMessage, object.get( "short_message" ) );
+    assertEquals( "GELF", object.get( "facility" ) );
+    assertEquals( null, object.get( "full_message" ) );
+    assertNotNull( object.get( "timestamp" ) );
+    assertEquals( null, object.get( "level" ) );
+    assertEquals( null, object.get( "file" ) );
+    assertEquals( null, object.get( "line" ) );
+    assertEquals( "localhost", object.get( "host" ) );
+    assertEquals( 5, object.size() );
+  }
+
+  @Test
+  public void messageToJson_WithNoDefaults()
+    throws Exception
+  {
+    final GelfMessage message = new GelfMessage();
+    final String myShortMessage = "MyShortMessage";
+    final String myFullMessage = "myFullMessage";
+    final String myFile = "myFile";
+    final long line = 42L;
+    final String funkyHost = "FunkyHost";
+    final String myFacility = "KnowckEmDownStorm";
+    message.setFacility( myFacility );
+    message.setShortMessage( myShortMessage );
+    message.setFullMessage( myFullMessage );
+    message.setLevel( SyslogLevel.ERR );
+    message.setFile( myFile );
+    message.setLine( line );
+    message.setHostname( funkyHost );
+    message.setJavaTimestamp( 3000 );
+    message.getAdditionalFields().put( "zang", "zing" );
+    final String json = new GelfEncoder( "localhost", true ).toJson( message );
+    assertNotNull( json );
+    final Map<String,Object> object = (Map<String,Object>) JSONValue.parse( json );
+
+    assertEquals( "1.0", object.get( "version" ) );
+    assertEquals( myShortMessage, object.get( "short_message" ) );
+    assertEquals( myFullMessage, object.get( "full_message" ) );
+    assertEquals( myFacility, object.get( "facility" ) );
+    assertEquals( "3", object.get( "timestamp" ) );
+    assertEquals( 3L, object.get( "level" ) );
+    assertEquals( myFile, object.get( "file" ) );
+    assertEquals( line, object.get( "line" ) );
+    assertEquals( funkyHost, object.get( "host" ) );
+    assertEquals( "zing", object.get( "_zang" ) );
+    assertEquals( 10, object.size() );
+/*
+
+    if( null != fullMessage )
+    {
+      map.put( "full_message", fullMessage );
+    }
+
+    final Long timestamp = message.getJavaTimestamp();
+    map.put( "timestamp", encodeTimestamp( null != timestamp ? timestamp : System.currentTimeMillis() ) );
+    final String facility = message.getFacility();
+    map.put( "facility", null != facility ? facility : DEFAULT_FACILITY );
+
+    final SyslogLevel level = message.getLevel();
+    if( null != level )
+    {
+      map.put( "level", level.ordinal() );
+    }
+
+    final String file = message.getFile();
+    if( null != file )
+    {
+      map.put( "file", file );
+    }
+    final Long line = message.getLine();
+    if( null != line )
+    {
+      map.put( "line", line );
+    }
+
+    final String hostname = message.getHostname();
+    map.put( "host", null == hostname ? _hostname : hostname );
+
+    final Map<String, Object> fields = message.getAdditionalFields();
+    for( final Map.Entry<String, Object> entry : fields.entrySet() )
+    {
+      final String key = entry.getKey();
+      if( !key.equals( ID_NAME ) )
+      {
+        map.put( "_" + key, entry.getValue() );
+      }
+    }
+*/
+
+  }
+
   @Test
   public void generateMessageID()
     throws Exception
