a31b05bbdade88176d1d0f350260b1269dc134ba
Peter Donald <peter@realityforge.org>
2012-04-24 22:17:43 +1000

    Remove unused import

--- src/main/java/gelf4j/GelfConnection.java
+++ src/main/java/gelf4j/GelfConnection.java
@@ -3,7 +3,6 @@ package gelf4j;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.security.MessageDigest;
 import java.util.List;
