e20f21072d28df8676c374e5d3f7a5d96a669515
Peter Donald <peter@realityforge.org>
2012-04-25 12:43:01 +1000

    Add some basic tests around the jdk logging handler

---
+++ src/test/java/gelf4j/logging/GelfHandlerTest.java
@@ -0,0 +1,112 @@
+package gelf4j.logging;
+
+import gelf4j.GelfMessage;
+import gelf4j.GelfTargetConfig;
+import gelf4j.SyslogLevel;
+import gelf4j.TestGelfConnection;
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.net.InetAddress;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GelfHandlerTest
+{
+  @Test
+  public void configureSetsUpLoggerCorrectly()
+    throws Exception
+  {
+    final String hostName = InetAddress.getLocalHost().getCanonicalHostName();
+    final String facility = "LOG4J";
+
+    final String prefix = GelfHandler.class.getName();
+    final String configData =
+      "handlers = " + prefix + "\n" +
+      ".level = FINE\n" +
+      "\n" +
+      prefix + ".host=" + hostName + "\n" +
+      prefix + ".port=1998\n" +
+      prefix + ".originHost=" + hostName + "\n" +
+      prefix + ".facility=" + facility + "\n" +
+      prefix + ".compressedChunking=false\n" +
+      prefix + ".level=FINE\n" +
+      prefix + ".additionalFields={\"thread_id\": \"threadId\", \"threadName\": \"threadName\", \"timestamp_in_millis\": \"timestampMs\", \"logger_name\": \"loggerName\", \"SourceClassName\": \"SourceClassName\", \"SourceMethodName\": \"SourceMethodName\", \"exception\": \"exception\"}\n" +
+      prefix + ".additionalData={\"environment\": \"DEV\", \"application\": \"MyAPP\"}\n" +
+      "\n";
+
+    LogManager.getLogManager().reset();
+    LogManager.getLogManager().readConfiguration( new ByteArrayInputStream( configData.getBytes() ) );
+
+    //PropertyConfigurator.configure( properties );
+    final Logger logger = Logger.getLogger( "ZipZipHooray" );
+    final Handler[] handlers = logger.getParent().getHandlers();
+    assertEquals( 1, handlers.length );
+    final Handler handler = handlers[ 0 ];
+    assertTrue( handler instanceof GelfHandler );
+    final GelfHandler gelfHandler = (GelfHandler) handler;
+    final GelfTargetConfig config = gelfHandler.getConfig();
+
+    assertEquals( hostName, config.getOriginHost() );
+    assertEquals( hostName, config.getHost() );
+    assertEquals( facility, config.getFacility() );
+    assertEquals( 1998, config.getPort() );
+    assertEquals( false, config.isCompressedChunking() );
+    assertEquals( 2, config.getAdditionalData().size() );
+    assertEquals( "DEV", config.getAdditionalData().get( "environment" ) );
+    assertEquals( "MyAPP", config.getAdditionalData().get( "application" ) );
+
+    assertEquals( 7, config.getAdditionalFields().size() );
+    assertEquals( "threadId", config.getAdditionalFields().get( "thread_id" ) );
+    assertEquals( "threadName", config.getAdditionalFields().get( "threadName" ) );
+    assertEquals( "timestampMs", config.getAdditionalFields().get( "timestamp_in_millis" ) );
+    assertEquals( "loggerName", config.getAdditionalFields().get( "logger_name" ) );
+    assertEquals( "SourceClassName", config.getAdditionalFields().get( "SourceClassName" ) );
+    assertEquals( "SourceMethodName", config.getAdditionalFields().get( "SourceMethodName" ) );
+    assertEquals( "exception", config.getAdditionalFields().get( "exception" ) );
+
+    // set up mock connection
+    final TestGelfConnection connection = new TestGelfConnection( config );
+    final Field field = gelfHandler.getClass().getDeclaredField( "_connection" );
+    field.setAccessible( true );
+    field.set( gelfHandler, connection );
+
+    final long then = System.currentTimeMillis();
+    final String smallTextMessage = "HELO";
+    logger.fine( smallTextMessage );
+
+    GelfMessage message = connection.getLastMessage();
+    assertEquals( smallTextMessage, message.getShortMessage() );
+    assertEquals( smallTextMessage, message.getFullMessage() );
+    assertEquals( SyslogLevel.DEBUG, message.getLevel() );
+    assertEquals( facility, message.getFacility() );
+    assertEquals( hostName, message.getHostname() );
+    assertEquals( null, message.getFile() );
+    assertNull( message.getLine() );
+    // The message is in the last second
+    assertTrue( message.getJavaTimestamp() - then < 1000 );
+
+    assertEquals( logger.getName(), message.getAdditionalFields().get( "logger_name" ) );
+    assertEquals( null, message.getAdditionalFields().get( "exception" ) );
+    assertNotNull( message.getAdditionalFields().get( "thread_id" ) );
+    assertTrue( ( (Long) message.getAdditionalFields().get( "timestamp_in_millis" ) ) - then < 1000 );
+    assertEquals( null, message.getAdditionalFields().get( "ip_address" ) );
+    assertEquals( Thread.currentThread().getName(), message.getAdditionalFields().get( "threadName" ) );
+    assertEquals( GelfHandlerTest.class.getName(), message.getAdditionalFields().get( "SourceClassName" ) );
+    assertEquals( "configureSetsUpLoggerCorrectly", message.getAdditionalFields().get( "SourceMethodName" ) );
+
+    assertEquals( "DEV", message.getAdditionalFields().get( "environment" ) );
+    assertEquals( "MyAPP", message.getAdditionalFields().get( "application" ) );
+
+    logger.log( Level.SEVERE, smallTextMessage, new Exception() );
+    message = connection.getLastMessage();
+    assertEquals( SyslogLevel.ERR, message.getLevel() );
+    assertNotNull( message.getAdditionalFields().get( "exception" ) );
+
+    // Force the closing of the appender
+    LogManager.getLogManager().reset();
+  }
+}
